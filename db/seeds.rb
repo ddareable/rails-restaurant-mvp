54a341d5758b1e7107989385e3ee92e9aaeec92d
Dara Schaetzle <info@dara-schaetzle.de>
2021-02-17 15:54:53 +0100

    created restaurant list MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurant1 = Restaurant.create!(name:"Chez Santi", address:"Paris")
+restaurant2 = Restaurant.create!(name:"Chez Romeo", address:"Liverpool")
